dad9f6e36a5277c253191598bd128eab9fc41b84
Max Otta <max.otta@gmail.com>
2022-05-05 03:25:28 +0200

    Added first two steps

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # iac-step-by-step
 IaC (Infrastructure as Code) Step by Step
+
+This repository contains examples used in practical labs related to cloud technologies at the Department of Computer Science and Engineering - University of West Bohemia, Pilsen, CZ
+
+## Installing Terraform
+
+https://learn.hashicorp.com/tutorials/terraform/install-cli
+
+## Setting up OpenNebula as Provider
+
+## Provided examples
+
+* Step 01 - very basic initial example
+* Step 02 - remote-exec script provisioner
+* Step 03 - create similar VMs
+
---
+++ step-01/variables.tf
@@ -0,0 +1,30 @@
+variable "one_endpoint"  {
+    description = "Open Nebula endpoint URL"
+    default = "https://your-opennebula-server/RPC2"
+}
+variable "one_username"  {
+    description = "Open Nebula username"
+}
+variable "one_password"  {
+    description = "Open Nebula login token"
+}
+variable "vm_ssh_pubkey" {
+    description = "SSH public key used for login as root into the VM instance"
+}
+variable "vm_startup_script" {
+    description = "VM Startup script"
+}
+variable "vm_imagedatastore_id" {
+    description = "Open Nebula datastore ID"
+    default = 109 # => "nuada_ssd"
+}
+variable "vm_network_id" {
+    description = "ID of the virtual network to attach to the virtual machine"
+    default = 3 # => "vlan173"
+}
+variable "vm_image_name" {
+    description = "VM OS image name"
+}
+variable "vm_image_url"  {
+    description = "VM OS image URL"
+}
